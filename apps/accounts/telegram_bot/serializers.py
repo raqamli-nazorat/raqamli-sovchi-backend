3bd1f6f65f3919aa6ee7b8df11b95b2018119798
Abdulbosit Alijonov <alijonov.me@gmail.com>
2026-07-30 13:28:11 +0500

    feat: Add phone_validator (+998XXXXXXXXX) to User model and serializers

--- apps/accounts/telegram_bot/serializers.py
+++ apps/accounts/telegram_bot/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
+
 from apps.core.base.serializers import BaseModelSerializer
+from apps.core.utils.validators import phone_validator
 from .models import LoginCode
 
+
 class LoginCodeSerializer(BaseModelSerializer):
+    phone_number = serializers.CharField(validators=[phone_validator])
+
     class Meta:
         model = LoginCode
         fields = ["id", "phone_number", "is_used", "expires_at", "created_at"]
         read_only_fields = ["id", "is_used", "expires_at", "created_at"]
 
+
 class VerifyCodeSerializer(serializers.Serializer):
-    phone_number = serializers.CharField()
+    phone_number = serializers.CharField(validators=[phone_validator])
     code = serializers.CharField(max_length=6, min_length=6)
