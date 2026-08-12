1b2ba06be07f04646e37ebf9e38cba6e3d386846
Abdulbosit Alijonov <alijonov.me@gmail.com>
2026-08-12 12:50:51 +0500

    fix(migrations): set explicit dependency on 0014_alter_profile_options in 0017_merge_migrations to prevent leaf node conflicts on production

--- apps/accounts/profiles/migrations/0017_merge_migrations.py
+++ apps/accounts/profiles/migrations/0017_merge_migrations.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("profiles", "0015_alter_profile_weight"),
+        ("profiles", "0014_alter_profile_options"),
         ("profiles", "0016_alter_representativeinfo_kinship"),
     ]
 
